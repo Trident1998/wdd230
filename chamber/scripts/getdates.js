6dc0670e4604908a57a9ac4683b3adb7984c7353
Dmytro <dima4994@gmail.com>
2023-10-07 23:40:02 +0300

    04 Assignment: Chamber Home Page Build

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,6 @@
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
