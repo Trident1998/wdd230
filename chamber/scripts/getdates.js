3ed9a9e4a33d5555c66fe84d184352c714cfcb1a
Dmytro <dima4994@gmail.com>
2023-10-21 18:31:57 +0300

    06 Assignment: Responsive Design

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -85,17 +85,18 @@ futures.textContent = futuresProperties;
 
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
+const body = document.querySelector("body");
 const sections = document.querySelectorAll("section")
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#001f3f";
+		body.style.background = "#001f3f";
 		main.style.color = "#fbfaf8";
 		sections.forEach(section => {
             section.style.background = "#333333";
         });		modeButton.textContent = "🔆";
 	} else {
-		main.style.background = "#fbfaf8";
+		body.style.background = "#fbfaf8";
 		main.style.color = "#001f3f";
 		sections.forEach(section => {
             section.style.background = "#eeeeee";
