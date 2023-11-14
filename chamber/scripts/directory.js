0941008a58adb5c6759ce61e6868a802806b7c58
Dmytro <dima4994@gmail.com>
2023-11-14 18:43:58 +0200

    09: Chamber Project Milestone - The Directory Page

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,50 @@
+document.querySelector("main").style.display = "block";
+
+const baseURL = "http://127.0.0.1:5500/wdd230/";
+const membersURL = "http://127.0.0.1:5500/wdd230/chamber/data/members.json";
+
+async function getMembers() {
+    const response = await fetch(membersURL);
+    const data = await response.json();
+    displayMembers(data.members);
+    console.log(data);
+}
+
+function displayMembers(members) {
+    const article = document.querySelector(".cards");
+    members.forEach(member => {
+        const section = document.createElement("section");
+        section.classList.add("card");
+
+        const h3 = document.createElement("h4")
+        const img = document.createElement("img");
+        const h2 = document.createElement("h3")
+        const a = document.createElement("a");
+        const p = document.createElement("p");
+
+
+        img.src = member.logo;
+        img.alt = `${member.name} logo`;
+
+        h2.textContent = member.name;
+        h3.textContent = member.membership_level;
+
+        a.href = member.link;
+        a.textContent = "Go to the Site";
+        a.target = "_blank";
+
+        p.textContent = `${member.staffs_amount} specialists`;
+        
+        section.appendChild(h2);
+        section.appendChild(img);
+        section.appendChild(h3);
+        section.appendChild(p);
+        section.appendChild(a);
+
+        article.appendChild(section)
+
+    });
+}
+  
+  
+  getMembers();
