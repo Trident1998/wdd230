64e2f1a28dd764f3e3e12a826d86fa4b402e992f
Dmytro <dima4994@gmail.com>
2023-11-25 20:25:37 +0200

    fix

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,80 +1,85 @@
-const info = document.querySelector(".info");
-const img = document.querySelector(".weather-img");
-const p = document.querySelector(".weather-descr")
-
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=50.4500&lon=30.5230&units=imperial&appid=674f4e29a0f93bfda31fbafaf123ec0e';
 const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=50.4500&lon=30.5230&units=imperial&appid=674f4e29a0f93bfda31fbafaf123ec0e';
 
 
-async function apiFetch() {
-    try {
+async function getWeatherData() {
+  try {
     const response = await fetch(url);
-    const forecastResponse = await fetch(forecastUrl);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+  }
+}
 
-    if (response.ok && forecastResponse.ok) {
-        const data = await response.json();
-        const forecastData = await forecastResponse.json();
-        console.log(data);
+async function displayCurrentWeather() {
+    const weatherData = await getWeatherData();
+    const currentWeather = document.querySelector('.current-weather');
+    const currentTempElement = document.getElementById('current-temp');
+    const currentDescriptionElement = document.getElementById('current-description');
+    const iconElement = document.createElement('img');
+    iconElement.classList.add("current-icon");
+  
+    if (weatherData) {
+      currentTempElement.textContent = `${weatherData.main.temp}°F`;
+      currentDescriptionElement.textContent = weatherData.weather[0].description;
+  
+      const iconCode = weatherData.weather[0].icon;
+      const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+      iconElement.src = iconUrl;
+      iconElement.alt = weatherData.weather[0].description;
 
-        displayResults(data); 
+      const h2Element = currentWeather.querySelector('h2');
 
-        displayForecastResults(forecastData); 
-    } else {
-        throw Error(await response.text());
+      currentWeather.insertBefore(iconElement, h2Element.nextSibling);
+
     }
+  }
+
+async function displayForecastWithDetails() {
+    try {
+      const response = await fetch(forecastUrl);
+      const data = await response.json();
+      const forecastGrid = document.getElementById('forecast-grid');
+  
+      if (data && data.list) {
+        const threeDayForecast = data.list.filter((item) => item.dt_txt.includes('12:00:00'));
+        let i = 0;
+  
+        threeDayForecast.forEach((forecastItem) => {
+            if(i < 3){
+                const date = new Date(forecastItem.dt * 1000);
+                const day = date.toLocaleDateString('en-US', { weekday: 'short' });
+                const iconCode = forecastItem.weather[0].icon;
+                const iconUrl = `https://openweathermap.org/img/w/${iconCode}.png`;
+                const minTemp = forecastItem.main.temp_min;
+                const maxTemp = forecastItem.main.temp_max;
+        
+                const forecastElement = document.createElement('div');
+                forecastElement.classList.add('forecast-item');
+        
+                const iconElement = document.createElement('img');
+                iconElement.classList.add("current-icon");
+                iconElement.src = iconUrl;
+                iconElement.alt = forecastItem.weather[0].description;
+        
+                const dataElement = document.createElement('div');
+                dataElement.classList.add('forecast-data');
+                dataElement.innerHTML = `<p>${minTemp}°F / ${maxTemp}°F</p><p>${day}</p>`;
+        
+                forecastElement.appendChild(iconElement);
+                forecastElement.appendChild(dataElement);
+                forecastGrid.appendChild(forecastElement);
+                i++;
+            
+            }
+          
+        });
+      }
     } catch (error) {
-        console.log(error);
+      console.error('Error fetching forecast:', error);
     }
-}
-
-function displayResults(data) {
-    const temp = data.main.temp;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    const description = data.weather[0].main;
-
-    img.src = iconsrc;
-    img.alt = data.weather[0].description;
-    img.style.display = "inline";
-
-    p.appendChild(document.createTextNode(`${temp}°F - ${description}`));
-}
-
-function displayForecastResults(data) {
-    const today = new Date();
-    const tommorow = new Date();
-    const thirdDay = new Date();
-    tommorow.setDate(today.getDate() + 1);
-    thirdDay.setDate(thirdDay.getDate() + 3);
-
-
-    const groupedByDate = getMapByDate(data);
-    
-    groupedByDate.forEach((value, key) => {
-        console.log(`Date: ${key}`);
-        console.log(value); // List of entities for that date
-      });
-
-}
-
-function getMapByDate(data) {
-    const groupedByDate = new Map();
-
-    data.list.forEach(entity => {
-    const dateTime = entity.dt_txt.split(' ');
-    const date = dateTime[0];
-
-    if (!groupedByDate.has(date)) {
-        groupedByDate.set(date, []);
-    }
-
-    groupedByDate.get(date).push(entity);
-    });
-
-    return groupedByDate;
-}
-
-const getFormatedDate = (date) => {
-    return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
-}
-
-apiFetch();
+  }
+  
+displayCurrentWeather();
+displayForecastWithDetails();
