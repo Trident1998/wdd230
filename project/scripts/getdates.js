1551824f67d40600b287a11ea17222af411808bd
Dmytro <dima4994@gmail.com>
2023-12-12 22:34:53 +0200

    project

--- project/scripts/getdates.js
+++ project/scripts/getdates.js
@@ -4,3 +4,19 @@ const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit
 const formattedDate = lastModified.toLocaleDateString(undefined, options);
 
 document.getElementById('lastModified').innerHTML = formattedDate;
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
